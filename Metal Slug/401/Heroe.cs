26b83656dde2ae95749e31bcdfe64999d38ed1bb
AlexanderMartinG <32973849+AlexanderMartinG@users.noreply.github.com>
2017-11-28 16:26:14 -0600

    Add files via upload

---
+++ Metal Slug/401/Heroe.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heroe : Humanoides {
+
+	[Tooltip ("Determina si el personaje actualmente esta siendo controlado")]
+	public bool esControlado; // Determina si el personaje actualmente esta siendo controlado
+
+	// Use this for initialization
+	new protected void Start () {
+		base.Start (); // Metodo Start de clase humanoides
+	}
+	
+	// Update is called once per frame
+	protected void Update () {
+		// Verificar si el personaje esta siendo controlado
+		if (esControlado) {
+
+			if (Input.GetKey ("a")) { // movimiento hacia la izquierda
+				miRB.velocity = new Vector2 (-velocidadMovimiento,miRB.velocity.y);
+				malla3D.localRotation = Quaternion.Euler (0, 180, 0);
+			}
+			if (Input.GetKey ("d")) { // movimiento hacia la derecha
+				miRB.velocity = new Vector2 (velocidadMovimiento,miRB.velocity.y);
+				malla3D.localRotation = Quaternion.Euler (0, 0, 0);
+
+			}
+
+			if (Input.GetKeyDown ("space") && Grounded()) { // Ejecutar un salto con la funcion Jump.
+				Jump();
+			}
+
+			if (Input.GetKey ("s") && Input.GetKeyDown ("space")) { //Atravezar plataforma
+				StartCoroutine (AtravezarPlataforma ());
+			}
+		}	
+	}
+
+	IEnumerator AtravezarPlataforma(){ // Desactiva el colisionador por 0.5 segundos
+		miColisionador.enabled = false;
+		yield return new WaitForSeconds (0.5f);
+		miColisionador.enabled = true;
+	}
+
+}
---
+++ Metal Slug/401/HeroeArtillero.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroeArtillero : Heroe {
+
+	[Tooltip("prefab de bala a instanciar en cada disparo")]
+	public GameObject bala; //prefab de bala a instanciar en cada disparo
+
+	[Tooltip("posicion donde se instanciara la bala")]
+	public Transform canonDePistola; // posicion donde se instanciara la bala
+
+	[Tooltip("Pivote para apuntar la pistola")]
+	public Transform hombro;
+
+	// Use this for initialization
+	new void Start () {
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	new void Update () {
+		base.Update ();
+
+		if (Input.GetMouseButtonDown (0)) { // Disparar
+			Disparar ();
+		}
+
+		if (Input.GetKey ("w")) { // Apuntar hacia Arriba
+			hombro.localRotation = Quaternion.Euler (0, 0, 90);
+		}
+
+		if (Input.GetKey ("s")) { // Apuntar hacia Arriba
+			hombro.localRotation = Quaternion.Euler (0, 0, -90);
+		}
+
+		if (!Input.GetKey ("w") && !Input.GetKey ("s")) { // Reiniciar angulo de hombro
+			hombro.localRotation = Quaternion.Euler (0, 0, 0);
+		}
+
+	}
+
+	//Funcion que instancia un clon de una bala
+	void Disparar(){
+		Instantiate (bala, canonDePistola.position, hombro.rotation);
+	}
+}
